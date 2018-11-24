5459ff146ea30d2cbf1459aee62d24c49125bf06
Esteban Buniak <buniakesteban@gmail.com>
2018-11-23 23:04:24 -0300

    agrego navbar

--- src/index.js
+++ src/index.js
@@ -7,8 +7,5 @@ import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-import Contador from "./componentes/contador";
-import EventosInteresantes from "./componentes/eventosInteresantes";
-
-ReactDOM.render(<EventosInteresantes />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
